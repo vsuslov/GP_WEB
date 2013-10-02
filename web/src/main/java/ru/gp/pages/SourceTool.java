a85efe277cd22c7af0db2325a8216f6c7538762f
Suslov Vadim <presvetorianec@gmail.com>
2013-10-02 16:44:35 +0600

    I've added file uploader to the site

--- web/src/main/java/ru/gp/pages/SourceTool.java
+++ web/src/main/java/ru/gp/pages/SourceTool.java
@@ -1,13 +1,27 @@
 package ru.gp.pages;
 
+import java.io.File;
+
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.upload.services.UploadedFile;
 
 import ru.gp.components.AbstractComponent;
 
 @Import(stylesheet = "context:css/tools.css")
 public class SourceTool extends AbstractComponent {
+	@Property
+	@Persist
+	private UploadedFile uploader;
 
-	// @Property
-	// private UploadedFile uploader;
+	@Property
+	@Persist
+	private String name;
 
+	public void onSuccess() {
+		File copied = new File("d:\\distr\\" + uploader.getFileName());
+
+		uploader.write(copied);
+	}
 }
